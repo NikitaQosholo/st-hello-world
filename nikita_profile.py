922d1bb7657c905c840573d9704503e255e04989
Nikita Qosholo <135969922+NikitaQosholo@users.noreply.github.com>
2025-01-29 14:31:45 +0200

    Update nikita_profile.py

--- nikita_profile.py
+++ nikita_profile.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
+import time
 
 # Title of the app
-st.title("Researcher Profile Page")
-
-st.image('Nikita_profile_photo.jpeg')
-
+st.title("Nikita's Profile Page")
 # Collect basic information
 name = "Nikita Qosholo"
 field = "Oceanography"
@@ -14,6 +12,20 @@ institution = "University of Cape Town"
 # Display basic profile information
 st.header("Researcher Overview")
 st.write(f"**Name:** {name}")
+st.image('Nikita_profile_photo.jpeg')
+
+st.header("Researcher Overview")
+description = """
+Master of Science student in Physical Oceanography with a strong focus on characterizing surface ocean variability using 
+observational data. Passionate about advancing understanding of ocean dynamics through rigorous analysis and research. 
+"""
+
+def stream_data():
+    for word in description.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
+# Display basic profile information
 st.write(f"**Field of Research:** {field}")
 st.write(f"**Institution:** {institution}")
 
